fafe01a9b150b0efed76ee92da2c1123f10901d5
frankchm <54816788+frankchm@users.noreply.github.com>
2021-04-29 22:40:31 -0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Admin</title>
+	</head>
+	<body>
+		<form action="admin_roles.php"><input type="submit" value="Admin: Board Members"/></form>
+		<form action="admin_members.php"><input type="submit" value="Admin: Members"/></form>
+		<form action="admin_guests.php"><input type="submit" value="Admin: Guests"/></form>
+		<form action="admin_attendance.php"><input type="submit" value="Admin: Attendance"/></form>
+		<form action="admin_memberTicketSales.php"><input type="submit" value="Admin: Member Ticket Sales"/></form>
+		<form action="admin_performances.php"><input type="submit" value="Admin: Performances"/></form>
+	</body>
+</html>
