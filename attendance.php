4c45376af4ed4137c9e4714d4f26e928f892b12c
CHarper4 <55903458+CHarper4@users.noreply.github.com>
2021-05-02 12:25:17 -0500

    Update attendance.php

--- attendance.php
+++ attendance.php
@@ -6,8 +6,9 @@
 	<form name="sort" method="get">
 		<label for="sort"></label>
 		<select name="sort">
-			<option value="first">First Name</option>
-			<option value="last">Last Name</option>
+			<option value="fname">First Name</option>
+			<option value="lname">Last Name</option>
+			<option value="pname">Preferred Name</option>
 			<option value="date">Date</option>
 		</select>
 		<input type="submit" value="Sort">
@@ -45,11 +46,14 @@
 			$filter = "";
 
 			switch($sort) {
-				case "first":
+				case "fname":
 					$filter = "ORDER BY Participant.first_name";
 					break;
-				case "last":
+				case "lname":
 					$filter = "ORDER BY Participant.last_name";
+					break;
+				case "pname":
+					$filter = "ORDER BY Participant.preferred_name";
 					break;
 				case "date":
 					$filter = "ORDER BY Attendance.attend_date";
