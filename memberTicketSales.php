1b9aee39b130c34f411e6271abb4d6f2a8751b4a
CHarper4 <55903458+CHarper4@users.noreply.github.com>
2021-05-02 16:15:54 -0500

    Update memberTicketSales.php

--- memberTicketSales.php
+++ memberTicketSales.php
@@ -1,5 +1,4 @@
 <html>
-	<form action="Index.php"><input type="submit" value="Home"/></form>
 	<head>
 		<title>Member Sales</title>
 		<link rel="stylesheet" href="style.css">
@@ -111,5 +110,8 @@
 			?>
 		</tbody>
 		</table>
+		<form action="index.php" method="post">
+			<input type="submit" style="color:white;background-color:blue" value="Home"/>
+		</form>
 	</body>
 </html>
