5c8d22a7b5b342b5dc1018e662253c9fd284d905
frankchm <54816788+frankchm@users.noreply.github.com>
2021-05-01 21:06:10 -0500

    Update Index.php
    Latest Update 5/1/2021

--- Index.php
+++ Index.php
@@ -1,7 +1,6 @@
 <html>
 	<head>
 		<title>GPH Database</title>
-		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<form action="roles.php"><input type="submit" value="Board Members"/></form>
@@ -9,6 +8,8 @@
 		<form action="guests.php"><input type="submit" value="Guests"/></form>
 		<form action="attendance.php"><input type="submit" value="Attendance"/></form>
 		<form action="memberTicketSales.php"><input type="submit" value="Member Ticket Sales"/></form>
-		<form action="performance.php"><input type="submit" value="Performances"/></form>
+		<form action="performances.php"><input type="submit" value="Performances"/></form>
+		<br><br>
+		<form action="admin.php"><input type="submit" style="color:white;background-color:red" value="Admin"/></form>
 	</body>
 </html>
